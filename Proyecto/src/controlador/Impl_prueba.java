6ea7f8ca1a227a5ba06f27d9d982c8489cb76c1e
ElAdminExe <thomas.rene.q.e@gmail.com>
2022-10-10 20:05:12 -0300

    Login-Menu
    Login:
              - header
              - ingresar usuario
              - ingresar contraseña
              - acceder
              - diseño
    Menu:
              - header
              - botones

---
+++ Proyecto/src/controlador/Impl_prueba.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controlador;
+
+/**
+ *
+ * @author thoma
+ */
+public class Impl_prueba{
+    public boolean login(String usuario,String contraseña){
+        //Inicio campos en blanco
+        boolean usuarioEnBlanco = usuario.equals("ingrese su correo") || usuario.equals("");
+        boolean contraseñaEnBlanco = contraseña.equals("************") || contraseña.equals("");
+        if(usuarioEnBlanco && contraseñaEnBlanco){
+            throw new NullPointerException("Es nesesario ingresar datos en los campos.");
+        }//Fin campos en blanco
+        
+        if(usuario.indexOf(" ")!=-1){
+            throw new NullPointerException("El usuario no debe contener espacios.");
+        }
+        if(usuario.equals("usuario")){
+            if(contraseña.equals("contraseña")){
+                return true;//usuario y contraseña correctos
+            }else{
+                //contraseña incorrecta
+            }
+        }else{
+            //usuario incorrecto
+        }
+        
+        return false;
+    }
+
+}
