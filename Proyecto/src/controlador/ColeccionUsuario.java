435bb8a41cb6acaadd1db4c9e4fdf4b7665893bd
Mredo13 <114360114+Mredo13@users.noreply.github.com>
2022-10-18 11:35:27 -0300

    Atributos modelo

--- Proyecto/src/controlador/ColeccionUsuario.java
+++ Proyecto/src/controlador/ColeccionUsuario.java
@@ -1,6 +1,14 @@
 package controlador;
 
+import Modelo.Usuario;
+import java.awt.List;
+import java.util.ArrayList;
+
 
 public class ColeccionUsuario {
-    
+    List <Usuario> lista;
+
+    public ColeccionUsuario() {
+        this.lista = new ArrayList <>();
+    }
 }
